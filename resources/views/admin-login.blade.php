2d65da1436ec652890a313760d86ad709c43ae55
Pragya <pragyatripathy3012@gmail.com>
2022-12-15 15:41:54 +0100

    change admin Image

--- resources/views/admin-login.blade.php
+++ resources/views/admin-login.blade.php
@@ -18,7 +18,7 @@
     <div class="container py-5 h-100">
       <div class="row d-flex align-items-center justify-content-center h-100">
         <div class="col-md-8 col-lg-7 col-xl-6">
-          <img src="../assets/images/12za.jpg"
+          <img src="../assets/images/admin.png"
             class="img-fluid" alt="energy image">
         </div>
         <div class="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
